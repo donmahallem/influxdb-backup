c618704ad07bc4090c030c8173b9f20276531f2e
donmahallem <donmahallem@users.noreply.github.com>
2025-02-15 18:56:06 +0100

    fix: use aws cli

--- backup.sh
+++ backup.sh
@@ -4,4 +4,4 @@ echo "$backup_dir"
 influx backup --host=$BACKUP_HOST --token=$BACKUP_TOKEN "./$backup_dir"
 tar -cvzf - "./$backup_dir" | gpg -c --batch --passphrase "$BACKUP_PASSWORD" > "$backup_dir.tar.gz.gpg"
 rm -rf "./$backup_dir"
-aws s3 cp "./$backup_dir.tar.gz.gpg" "s3://$BACKUP_BUCKET/influxb_backup/$backup_dir.tar.gz.gpg" --expires 2025-03-14T20:30:00Z
+aws s3 cp "./$backup_dir.tar.gz.gpg" "s3://$BACKUP_BUCKET/influxb_backup/$backup_dir.tar.gz.gpg"
